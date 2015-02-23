59d7c82f94cde7c64d77a21ef4ab74b9e760beec
glajman <vladimirhadzic@yahoo.co.uk>
2015-02-23 22:11:36 +0100

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module SampleApp
-  class Application < Rails::Application
+    class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +21,9 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-  end
+        config.active_record.raise_in_transactional_callbacks = true
+
+    # Include the authenticity token in remote forms.
+        config.action_view.embed_authenticity_token_in_remote_forms = true
+    end
 end
